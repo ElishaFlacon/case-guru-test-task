f95d1f1bc8780da977854f6c465b0b1ef7de9312
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-08-20 16:50:28 +0500

    add more validations

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -16,31 +16,31 @@ export class AuthService {
     // регистрирует пользователя, которого необходимо подтвердить и добавить в него пароль и логин
     async registration(userDto: CreateUserDto) {
         const availableLink = v4();
-        const user = await this.usersService.create({ ...userDto, availableLink });
-        return user;
+        const birthdayMonth = new Date(userDto.birthday).getMonth() + 1
+        const user = await this.usersService.create({ ...userDto, availableLink, birthdayMonth: String(birthdayMonth) });
+        return new FullUsersDto(user);
     }
 
     async login(userDto: UserDto) {
         const user = await this.validateUser(userDto);
         const token = await this.generateToken(user);
-        return { user, token };
-    }
-
-    async generateToken(userDto: UserDto) {
-        const payload = { id: userDto.id, login: userDto.login, username: userDto.username, role: userDto.role };
-        const token = this.jwtService.sign(payload);
-        return token;
+        return { user: new FullUsersDto(user), token };
     }
 
     // подтверждает пользователя по ссылке
     async confirmUser(userDto: ConfirmUserDto, availableLink: string) {
         const user = await this.usersService.getByLink(availableLink);
 
+        if (!user) {
+            throw new HttpException('Этого пользователя не существует!', HttpStatus.BAD_REQUEST);
+        }
+
         if (user.isAvailable) {
             throw new HttpException('Этот пользователь уже зарегистрирован!', HttpStatus.BAD_REQUEST);
         }
 
-        if (user.login === userDto.login) {
+        const validate_user = await this.usersService.getByLogin(userDto.login);
+        if (validate_user && validate_user.login === userDto.login) {
             throw new HttpException('Пользователь с таким логином уже существует!', HttpStatus.BAD_REQUEST);
         }
 
@@ -58,17 +58,28 @@ export class AuthService {
         });
         const token = await this.generateToken(newUser);
 
-        return { user: newUser, token };
+        return { user: new FullUsersDto(newUser), token };
+    }
+
+    async generateToken(userDto: UserDto) {
+        const payload = { id: userDto.id, login: userDto.login, username: userDto.username, role: userDto.role };
+        const token = this.jwtService.sign(payload);
+        return token;
     }
 
     // проверяем валидность пользователя
     private async validateUser(userDto: UserDto) {
         const user = await this.usersService.getByLogin(userDto.login);
-        const checkPasswords = await bcrypt.compare(userDto.password, user.password);
+
+        if (!user) {
+            throw new HttpException('Этого пользователя не существует!', HttpStatus.BAD_REQUEST);
+        }
 
         if (!user.isAvailable) {
             throw new UnauthorizedException({ message: 'Пользователь не подтвержден!' });
         }
+
+        const checkPasswords = await bcrypt.compare(userDto.password, user.password);
 
         if (!user || !checkPasswords) {
             throw new UnauthorizedException({ message: 'Неверный логин или пароль!' });
