f5b8cdf98216afbda44ae033668028f097b587ee
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-08-20 16:49:25 +0500

    add cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 async function startApp() {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
-
+    app.enableCors({
+        origin: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+    })
     await app.listen(PORT, () => console.log(`app started on http://localhost:${PORT}`));
 }
 
